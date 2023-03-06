15cc73c554c19f308eeb6d7afcac7e379aec5bd2
k1n0r4 <sejal.koshta2003@gmail.com>
2023-03-06 22:20:02 +0530

    Updated solve.py

--- docs/reversing/kalmarctf23/cyclechaser/solve.py
+++ docs/reversing/kalmarctf23/cyclechaser/solve.py
@@ -3,10 +3,8 @@ yy = b'\x00'
 zz= b'\xff'
 x = yy*16384+zz*9
 
-#io = process("./cyclechaser")#
 io = remote('3.123.91.129',13339)
 print('seed', io.recvline())
-#io.recvline()
 io.sendline(x)
 
 ret = io.recvline()
